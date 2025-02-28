b47519214853bd1b5e9a56df6f85822ee393800a
Karel Suchomel <karel.suchomel11@gmail.com>
2025-02-28 11:10:04 +0100

    Initial commit

--- src/features/tasks/screens/TasksScreen/index.tsx
+++ src/features/tasks/screens/TasksScreen/index.tsx
@@ -1,4 +1,5 @@
 import { View } from 'react-native'
+import { StyleSheet } from 'react-native-unistyles'
 
 import { Layout } from '~/theme/components/Layout'
 import { Text } from '~/theme/components/Text'
@@ -9,8 +10,17 @@ export function Tasks() {
     <Layout>
       <View style={[tokens.flex_1, tokens.align_center, tokens.justify_center, tokens.gap('lg')]}>
         <Text>Tasks</Text>
-        <View style={[tokens.w(50), tokens.h(50), tokens.bg('fillPrimary')]} />
+        <View style={[tokens.w(50), tokens.h(50), styles.test]} />
       </View>
     </Layout>
   )
 }
+
+const styles = StyleSheet.create((theme) => ({
+  test: {
+    backgroundColor: {
+      xs: theme.colors.purple,
+      md: theme.colors.red,
+    },
+  },
+}))
